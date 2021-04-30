d0349d91396d079e9ccd11937483344767cd0c32
DerCookieLiker <gsupersocke@gmail.com>
2021-04-30 14:44:32 +0200

    AABB Collision

--- Pong/main.cpp
+++ Pong/main.cpp
@@ -1,12 +1,14 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
+
+bool AABB(sf::RectangleShape &a, sf::RectangleShape &b);
 int main(){
 
     const int WINDOW_MAX_X = 1000;
     const int WINDOW_MAX_Y = 500;
 
     sf::RenderWindow window(sf::VideoMode(WINDOW_MAX_X, WINDOW_MAX_Y), "Pong!");
-    sf::RectangleShape shape({20, 20});
+    sf::RectangleShape ball({20, 20});
     sf::RectangleShape leftPlayer({20, 100});
     sf::RectangleShape rightPlayer({20, 100});
 
@@ -16,12 +18,14 @@ int main(){
     rightPlayer.setPosition({WINDOW_MAX_X - 40, (WINDOW_MAX_Y - 100) / 2});
     rightPlayer.setFillColor(sf::Color::White);
 
-    shape.setPosition({(WINDOW_MAX_X - 20) / 2, (WINDOW_MAX_Y - 20) / 2});
-    shape.setFillColor(sf::Color::White);
+    ball.setPosition({(WINDOW_MAX_X - 20) / 2, (WINDOW_MAX_Y - 20) / 2});
+    ball.setFillColor(sf::Color::White);
 
     sf::Clock current;
     float deltaTime;
     float speed = 200;
+    sf::Vector2f ballVec(1, 1);
+
     while (window.isOpen())
     {
         deltaTime = current.restart().asSeconds();
@@ -32,24 +36,50 @@ int main(){
                 window.close();
         }
 
-        if(sf::Keyboard::isKeyPressed(sf::Keyboard::W) && leftPlayer.getPosition().y > 0.0 ){
+        if( sf::Keyboard::isKeyPressed(sf::Keyboard::W) && leftPlayer.getPosition().y > 0.0 ){
             leftPlayer.move(0.0, -speed * deltaTime);
-        }else if(sf::Keyboard::isKeyPressed(sf::Keyboard::S) && leftPlayer.getPosition().y < WINDOW_MAX_Y - 100){
+        }else if( sf::Keyboard::isKeyPressed(sf::Keyboard::S) && leftPlayer.getPosition().y < WINDOW_MAX_Y - 100 ){
             leftPlayer.move(0.0, speed * deltaTime);
         }
-        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up) && rightPlayer.getPosition().y > 0.0 ){
+
+        if( sf::Keyboard::isKeyPressed(sf::Keyboard::Up) && rightPlayer.getPosition().y > 0.0 ){
             rightPlayer.move(0.0, -speed * deltaTime);
-        }else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down) && rightPlayer.getPosition().y < WINDOW_MAX_Y - 100){
+        }else if( sf::Keyboard::isKeyPressed(sf::Keyboard::Down) && rightPlayer.getPosition().y < WINDOW_MAX_Y - 100 ){
             rightPlayer.move(0.0, speed * deltaTime);
         }
 
+        if(AABB(leftPlayer, ball)){
+            ballVec.x *= -1;
+        }else if(AABB(rightPlayer, ball)){
+            ballVec.x *= -1;
+        }
+        if(ball.getPosition().y < 0.0 || ball.getPosition().y > WINDOW_MAX_Y - 20){
+            ballVec.y *= -1;
+        }else if(ball.getPosition().x < 0.0 || ball.getPosition().x > WINDOW_MAX_X - 20){
+            ball.setPosition({(WINDOW_MAX_X - 20) / 2, (WINDOW_MAX_Y - 20) / 2});
+            ballVec.x *= -1;
+        }
+        ball.move((ballVec.x * speed) * deltaTime, (ballVec.y * speed) * deltaTime);
+
         std::cout << "First Player[ Y: " << leftPlayer.getPosition().y << " ] "
-        << "Second Player[ Y: " << rightPlayer.getPosition().y << " ]"<< std::endl;
+                  << "Second Player[ Y: " << rightPlayer.getPosition().y << " ]"
+                  << "Collision: " << AABB(leftPlayer, ball) <<std::endl;
+
+
         window.clear();
-        window.draw(shape);
+        window.draw(ball);
         window.draw(leftPlayer);
         window.draw(rightPlayer);
         window.display();
     }
     return 0;
+}
+bool AABB(sf::RectangleShape &a, sf::RectangleShape &b){
+
+    if((a.getPosition().x < b.getPosition().x + b.getSize().x) &&
+       (a.getPosition().x + a.getSize().x > b.getPosition().x) &&
+       (a.getPosition().y < b.getPosition().y + b.getSize().y) &&
+       (a.getPosition().y + a.getSize().y > b.getPosition().y)) return true;
+        return false;
+
 }
