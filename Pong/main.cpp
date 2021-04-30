a556536223c4ed036c43561b2f637e35a8cfe27d
DerCookieLiker <gsupersocke@gmail.com>
2021-04-30 13:39:02 +0200

    playerMovement

--- Pong/main.cpp
+++ Pong/main.cpp
@@ -1,12 +1,30 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
+int main(){
 
-int main()
-{
-    sf::RenderWindow window(sf::VideoMode(200, 200), "Pong!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    const int WINDOW_MAX_X = 1000;
+    const int WINDOW_MAX_Y = 500;
+
+    sf::RenderWindow window(sf::VideoMode(WINDOW_MAX_X, WINDOW_MAX_Y), "Pong!");
+    sf::RectangleShape shape({20, 20});
+    sf::RectangleShape leftPlayer({20, 100});
+    sf::RectangleShape rightPlayer({20, 100});
+
+    leftPlayer.setPosition({20, (WINDOW_MAX_Y - 100) /2});
+    leftPlayer.setFillColor(sf::Color::White);
+
+    rightPlayer.setPosition({WINDOW_MAX_X - 40, (WINDOW_MAX_Y - 100) / 2});
+    rightPlayer.setFillColor(sf::Color::White);
+
+    shape.setPosition({(WINDOW_MAX_X - 20) / 2, (WINDOW_MAX_Y - 20) / 2});
+    shape.setFillColor(sf::Color::White);
+
+    sf::Clock current;
+    float deltaTime;
+    float speed = 200;
     while (window.isOpen())
     {
+        deltaTime = current.restart().asSeconds();
         sf::Event event;
         while (window.pollEvent(event))
         {
@@ -14,8 +32,23 @@ int main()
                 window.close();
         }
 
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::W) && leftPlayer.getPosition().y > 0.0 ){
+            leftPlayer.move(0.0, -speed * deltaTime);
+        }else if(sf::Keyboard::isKeyPressed(sf::Keyboard::S) && leftPlayer.getPosition().y < WINDOW_MAX_Y - 100){
+            leftPlayer.move(0.0, speed * deltaTime);
+        }
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up) && rightPlayer.getPosition().y > 0.0 ){
+            rightPlayer.move(0.0, -speed * deltaTime);
+        }else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down) && rightPlayer.getPosition().y < WINDOW_MAX_Y - 100){
+            rightPlayer.move(0.0, speed * deltaTime);
+        }
+
+        std::cout << "First Player[ Y: " << leftPlayer.getPosition().y << " ] "
+        << "Second Player[ Y: " << rightPlayer.getPosition().y << " ]"<< std::endl;
         window.clear();
         window.draw(shape);
+        window.draw(leftPlayer);
+        window.draw(rightPlayer);
         window.display();
     }
     return 0;
