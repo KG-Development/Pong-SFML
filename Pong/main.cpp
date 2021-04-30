2dfb1bb642eab1380662e12e48a4b558e228bf56
DerCookieLiker <gsupersocke@gmail.com>
2021-04-30 16:55:59 +0200

    bug fixes

--- Pong/main.cpp
+++ Pong/main.cpp
@@ -11,7 +11,6 @@ const int WINDOW_MAX_Y = 500;
 int main(){
 
     sf::RenderWindow window(sf::VideoMode(WINDOW_MAX_X, WINDOW_MAX_Y), "Pong!");
-
     sf::RectangleShape ball({20, 20});
     sf::RectangleShape leftPlayer({20, 100});
     sf::RectangleShape rightPlayer({20, 100});
@@ -29,6 +28,8 @@ int main(){
     float deltaTime;
     float speed = 200;
     float ballSpeed = 200;
+    float delay = 0;
+    int gameState = 0; //0 Pause //1 Run //2 Game Over only if one reach the score 10
     sf::Vector2f ballVec(1, 0);
 
     while (window.isOpen())
@@ -39,58 +40,79 @@ int main(){
         if(ballSpeed >= 200.1){
             ballSpeed -= deltaTime;
         }
+        if(delay > 0){
+            delay -= deltaTime;
+        }
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
                 window.close();
         }
 
-        if( sf::Keyboard::isKeyPressed(sf::Keyboard::W) && leftPlayer.getPosition().y > 20 ){
-            leftPlayer.move(0.0, -speed * deltaTime);
-        }else if( sf::Keyboard::isKeyPressed(sf::Keyboard::S) && leftPlayer.getPosition().y < WINDOW_MAX_Y - 120 ){
-            leftPlayer.move(0.0, speed * deltaTime);
-        }
-
-        if( sf::Keyboard::isKeyPressed(sf::Keyboard::Up) && rightPlayer.getPosition().y > 20 ){
-            rightPlayer.move(0.0, -speed * deltaTime);
-        }else if( sf::Keyboard::isKeyPressed(sf::Keyboard::Down) && rightPlayer.getPosition().y < WINDOW_MAX_Y - 120 ){
-            rightPlayer.move(0.0, speed * deltaTime);
-        }
-
-        if(AABB(leftPlayer, ball)){
-            if(ball.getPosition().y >= leftPlayer.getPosition().y + 66){
-                ballVec.y = 1;
-            }else if (ball.getPosition().y <= leftPlayer.getPosition().y + 33){
-                ballVec.y = -1;
+        switch(gameState){
+        case 0:
+            if(sf::Keyboard::isKeyPressed(sf::Keyboard::Escape) && delay <= 0){
+                gameState = 1;
+                delay = 0.1;
             }
-            ballVec.x *= -1;
-            ball.setPosition(ball.getPosition().x + ballVec.x, ball.getPosition().y);
-            ballSpeed = 250;
-        }else if(AABB(rightPlayer, ball)){
-            if(ball.getPosition().y >= rightPlayer.getPosition().y + 66){
-                ballVec.y = 1;
-            }else if (ball.getPosition().y <= rightPlayer.getPosition().y + 33){
-                ballVec.y = -1;
+            break;
+        case 2:
+            break;
+        case 1:
+                if(sf::Keyboard::isKeyPressed(sf::Keyboard::Escape) && delay <= 0){
+                        gameState = 0;
+                        delay = 0.1;
+                }
+                if( sf::Keyboard::isKeyPressed(sf::Keyboard::W) && leftPlayer.getPosition().y > 20 ){
+                    leftPlayer.move(0.0, -speed * deltaTime);
+                }else if( sf::Keyboard::isKeyPressed(sf::Keyboard::S) && leftPlayer.getPosition().y < WINDOW_MAX_Y - 120 ){
+                    leftPlayer.move(0.0, speed * deltaTime);
+                }
+
+                if( sf::Keyboard::isKeyPressed(sf::Keyboard::Up) && rightPlayer.getPosition().y > 20 ){
+                    rightPlayer.move(0.0, -speed * deltaTime);
+                }else if( sf::Keyboard::isKeyPressed(sf::Keyboard::Down) && rightPlayer.getPosition().y < WINDOW_MAX_Y - 120 ){
+                    rightPlayer.move(0.0, speed * deltaTime);
+                }
+
+                if(AABB(leftPlayer, ball)){
+                    if(ball.getPosition().y >= leftPlayer.getPosition().y + 66){
+                        ballVec.y = 1;
+                    }else if (ball.getPosition().y <= leftPlayer.getPosition().y + 33){
+                        ballVec.y = -1;
+                    }
+                    ballVec.x *= -1;
+                    ball.setPosition(ball.getPosition().x + ballVec.x, ball.getPosition().y);
+                    ballSpeed = 250;
+                }else if(AABB(rightPlayer, ball)){
+                    if(ball.getPosition().y >= rightPlayer.getPosition().y + 66){
+                        ballVec.y = 1;
+                    }else if (ball.getPosition().y <= rightPlayer.getPosition().y + 33){
+                        ballVec.y = -1;
+                    }
+                    ballVec.x *= -1;
+                    ball.setPosition(ball.getPosition().x + ballVec.x, ball.getPosition().y);
+                    ballSpeed = 250;
+                }
+                if(ball.getPosition().y < 20 || ball.getPosition().y > WINDOW_MAX_Y - 40){
+                    ballVec.y *= -1;
+                    ball.setPosition(ball.getPosition().x, ball.getPosition().y + ballVec.y);
+                }else if(ball.getPosition().x < 20 || ball.getPosition().x > WINDOW_MAX_X - 40){
+                    reset(leftPlayer, ball, rightPlayer);
+                    ballVec.x *= -1;
+                    ballVec.y = 0;
+                }
+                ball.move((ballVec.x * ballSpeed) * deltaTime, (ballVec.y * ballSpeed) * deltaTime);
+
+                std::cout << "First Player[ Y: " << leftPlayer.getPosition().y << " ] "
+                          << "Second Player[ Y: " << rightPlayer.getPosition().y << " ]"
+                          << "BallVec: [ X: " << ballVec.x << " Y: " << ballVec.y << " Speed: " << ballSpeed <<std::endl;
+
+
+
+
+            break;
             }
-            ballVec.x *= -1;
-            ball.setPosition(ball.getPosition().x + ballVec.x, ball.getPosition().y);
-            ballSpeed = 250;
-        }
-        if(ball.getPosition().y < 20 || ball.getPosition().y > WINDOW_MAX_Y - 40){
-            ballVec.y *= -1;
-            ball.setPosition(ball.getPosition().x, ball.getPosition().y + ballVec.y);
-        }else if(ball.getPosition().x < 20 || ball.getPosition().x > WINDOW_MAX_X - 40){
-            reset(leftPlayer, ball, rightPlayer);
-            ballVec.x *= -1;
-            ballVec.y = 0;
-        }
-        ball.move((ballVec.x * ballSpeed) * deltaTime, (ballVec.y * ballSpeed) * deltaTime);
-
-        std::cout << "First Player[ Y: " << leftPlayer.getPosition().y << " ] "
-                  << "Second Player[ Y: " << rightPlayer.getPosition().y << " ]"
-                  << "BallVec: [ X: " << ballVec.x << " Y: " << ballVec.y << " Speed: " << ballSpeed <<std::endl;
-
-
         window.clear();
         drawField(window);
         window.draw(ball);
